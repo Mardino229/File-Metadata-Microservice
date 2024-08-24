6364b083a175ccaeb48a570945cde441c4eca894
Mardino229 <maxdassi01@gmail.com>
2024-08-24 16:41:12 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const bodyParser = require('body-parser');
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single("upfile"), function (req, res,next) {
+  const file = req.file
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
